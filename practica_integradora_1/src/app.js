95d7778905a7da4b151c28de1f87538f345a08cb
renier perez <rainer991@gmail.com>
2024-05-21 17:55:02 -0400

    corregido variable de entorno

--- practica_integradora_1/src/app.js
+++ practica_integradora_1/src/app.js
@@ -7,9 +7,9 @@ import productsRouter from "./routes/products.router.js";
 import cartsRouter from "./routes/carts.router.js";
 import viewsRouter from "./routes/views.router.js";
 import __dirname from "./utils.js";
-// import dotenv from "dotenv";
-// dotenv.config();
-// console.log(process.env.MONGO_URL);
+import dotenv from "dotenv";
+dotenv.config();
+console.log(process.env.MONGO_URL);
 
 const app = express();
 const PORT = 8080;
@@ -19,22 +19,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
 
 mongoose
-  .connect(
-    "mongodb+srv://rainer:2025@cluster0.1fy9xjh.mongodb.net/ecommerce?retryWrites=true&w=majority&appName=Cluster0"
-  )
+  .connect(process.env.MONGO_URL)
   .then(() => {
-    console.log("Connectado a la base de datos");
+    console.log("Conectado a la base de datos");
   })
-  .catch((error) => {
-    console.error("Error connectando a la base de datos", error);
-  });
-
-// mongoose
-//   .connect(process.env.MONGO_URL)
-//   .then(() => {
-//     console.log("Conectado a la base de datos");
-//   })
-//   .catch((error) => console.error("Error en la conexion", error));
+  .catch((error) => console.error("Error en la conexion", error));
 
 app.engine("handlebars", handlebars.engine());
 app.set("views", __dirname + "/views");
